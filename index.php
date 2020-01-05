075af8a3f8bd99a60d651a00f0f0d5f32e18230c
HELDO  MARQUES <48698933+HeldoMarques@users.noreply.github.com>
2020-01-05 11:46:09 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,83 @@
+<?php
+session_start();
+include_once("conexao.php");
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>CONTROLE DE O.S ELETRICA LARA</title>		
+	</head>
+	<body>
+		<a href="cad_usuario.php">Cadastrar</a><br>
+		<a href="index.php">Listar</a><br>
+		<h1>Listar Usuário</h1>
+		<?php
+		if(isset($_SESSION['msg'])){
+			echo $_SESSION['msg'];
+			unset($_SESSION['msg']);
+		}
+		
+		//Receber o número da página
+		$pagina_atual = filter_input(INPUT_GET,'pagina', FILTER_SANITIZE_NUMBER_INT);		
+		$pagina = (!empty($pagina_atual)) ? $pagina_atual : 1;
+		
+		//Setar a quantidade de itens por pagina
+		$qnt_result_pg = 10;
+		
+		//calcular o inicio visualização
+		$inicio = ($qnt_result_pg * $pagina) - $qnt_result_pg;
+		
+		$result_usuarios = "SELECT * FROM usuarios LIMIT $inicio, $qnt_result_pg";
+		$resultado_usuarios = mysqli_query($conn, $result_usuarios);
+		while($row_usuario = mysqli_fetch_assoc($resultado_usuarios)){
+			echo "ID: " . $row_usuario['id'] . "<br>";
+			 echo "Nome: " . $row_usuario['nome'] . "<br>";
+			 echo "Endereço: " . $row_usuario['endereco'] . "<br>";
+			echo "cidade: " . $row_usuario['cidade'] . "<br>";
+			echo "casa: " . $row_usuario['casa'] . "<br>";
+			echo "apto: " . $row_usuario['apto'] . "<br>";
+			echo "vendedor: " . $row_usuario['vendedor'] . "<br>";
+			echo "prestador: " . $row_usuario['prestador'] . "<br>";
+			echo "valor: " . $row_usuario['valor'] . "<br>";
+			echo "material: " . $row_usuario['material'] . "<br>";
+			echo "servico: " . $row_usuario['servico'] . "<br>";
+			echo "E-mail: " . $row_usuario['email'] . "<br>";
+			echo "fixo: " . $row_usuario['fixo'] . "<br>";
+			echo "celular: " . $row_usuario['celular'] . "<br>";
+
+			echo "<a href='edit_usuario.php?id=" . $row_usuario['id'] . "'>Editar</a><br>";
+			echo "<a href='proc_apagar_usuario.php?id=" . $row_usuario['id'] . "'>Apagar</a><br><hr>";
+		}
+		
+		//Paginção - Somar a quantidade de usuários
+		$result_pg = "SELECT COUNT(id) AS num_result FROM usuarios";
+		$resultado_pg = mysqli_query($conn, $result_pg);
+		$row_pg = mysqli_fetch_assoc($resultado_pg);
+		//echo $row_pg['num_result'];
+		//Quantidade de pagina 
+		$quantidade_pg = ceil($row_pg['num_result'] / $qnt_result_pg);
+		
+		//Limitar os link antes depois
+		$max_links = 2;
+		echo "<a href='index.php?pagina=1'>Primeira</a> ";
+		
+		for($pag_ant = $pagina - $max_links; $pag_ant <= $pagina - 1; $pag_ant++){
+			if($pag_ant >= 1){
+				echo "<a href='index.php?pagina=$pag_ant'>$pag_ant</a> ";
+			}
+		}
+			
+		echo "$pagina ";
+		
+		for($pag_dep = $pagina + 1; $pag_dep <= $pagina + $max_links; $pag_dep++){
+			if($pag_dep <= $quantidade_pg){
+				echo "<a href='index.php?pagina=$pag_dep'>$pag_dep</a> ";
+			}
+		}
+		
+		echo "<a href='index.php?pagina=$quantidade_pg'>Ultima</a>";
+		
+		?>		
+	</body>
+</html>
